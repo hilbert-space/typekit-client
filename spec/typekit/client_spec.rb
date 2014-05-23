be78c6a8a57e952c6c5b9d519aefd2f10da33391
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-05-23 17:22:34 +0200

    Added a couple of specs for Client and a couple of VCR cassettes

--- spec/typekit/client_spec.rb
+++ spec/typekit/client_spec.rb
@@ -12,4 +12,24 @@ describe Typekit::Client do
       expect(subject).to respond_to(method)
     end
   end
+
+  describe '#get' do
+    context 'the resuest has been successfull' do
+      options = { vcr: { cassette_name: 'kits_ok' } }
+
+      it 'returns an array of appropriate Elements', options do
+        result = subject.get(:kits)
+        result.each { |r| expect(r).to be_kind_of(Typekit::Element::Kit) }
+      end
+    end
+
+    context 'the resuest has not been authorization' do
+      options = { vcr: { cassette_name: 'kits_unauthorized' } }
+
+      it 'raises an appropriate Error', options do
+        expect { subject.get(:kits) }.to \
+          raise_error(Typekit::Error, /(authentication|authorized)/i)
+      end
+    end
+  end
 end
