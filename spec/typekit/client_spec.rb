ce92185bf1d6972f21a0b8dfc0e14ba9fac30cf7
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-05-30 17:40:40 +0200

    Refactored a bit VCR cassettes in the Client spec; added a delete cassette

--- spec/typekit/client_spec.rb
+++ spec/typekit/client_spec.rb
@@ -2,35 +2,45 @@ require 'spec_helper'
 require 'typekit'
 
 describe Typekit::Client do
+  extend VCR::RSpec::Macros
+
   let(:token) { 'arbitrary' }
   let(:subject) { Typekit::Client.new(token: token) }
 
   describe '#index' do
     context 'when successful' do
-      options = { vcr: { cassette_name: 'index_kits_ok' } }
+      use_vcr_cassette 'index_kits_ok'
 
-      it 'returns Records', options do
+      it 'returns Records' do
         result = subject.index(:kits)
         expect(result.map(&:class).uniq).to eq([ Typekit::Record::Kit ])
       end
     end
 
     context 'when unauthorized' do
-      options = { vcr: { cassette_name: 'index_kits_unauthorized' } }
+      use_vcr_cassette 'index_kits_unauthorized'
 
-      it 'raises exceptions', options do
+      it 'raises exceptions' do
         expect { subject.index(:kits) }.to \
           raise_error(Typekit::Processing::Error, /Not authorized/i)
       end
     end
 
     context 'when found' do
-      options = { vcr: { cassette_name: 'show_families_calluna_found' } }
+      use_vcr_cassette 'show_families_calluna_found'
 
-      it 'returns the Response as is', options do
+      it 'returns the Response as is' do
         expect { subject.show(:families, 'calluna') }.not_to \
           raise_error
       end
+    end
+  end
+
+  describe '#delete' do
+    use_vcr_cassette 'delete_kits_xxx_ok'
+
+    it 'returns true' do
+      expect(subject.delete(:kits, 'xxx')).to be(true)
     end
   end
 end
