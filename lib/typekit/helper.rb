2612d5310e921bbaf054cf5a380a25ccd953d814
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-05-31 07:09:16 +0200

    Implemented basic pluralization/singularization

--- lib/typekit/helper.rb
+++ lib/typekit/helper.rb
@@ -22,6 +22,28 @@ module Typekit
       Rack::Utils.build_nested_query(prepare_parameters(parameters))
     end
 
+    def self.pluralize(name)
+      case name
+      when /^.*s$/
+        name
+      when /^(?<root>.*)y$/
+        "#{ Regexp.last_match(:root) }ies"
+      else
+        "#{ name }s"
+      end
+    end
+
+    def self.singularize(name)
+      case name
+      when /^(?<root>.*)ies$/
+        "#{ Regexp.last_match(:root) }y"
+      when /^(?<root>.*)s$/
+        Regexp.last_match(:root)
+      else
+        name
+      end
+    end
+
     private
 
     def self.prepare_parameters(parameters)
