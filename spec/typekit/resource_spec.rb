9a39793a67ec277726a97f15909963a4168f8427
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-06-13 18:21:09 +0200

    Added a spec for Resource.build

--- spec/typekit/resource_spec.rb
+++ spec/typekit/resource_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Typekit::Resource do
 
   describe '.identify' do
     resource_dictionary.each do |name, klass|
-      it "converts :#{ name } into :elment" do
+      it "converts :#{ name } into :element" do
         expect(subject_module.identify(name)).to eq(:element)
       end
 
@@ -33,6 +33,25 @@ RSpec.describe Typekit::Resource do
 
     it 'returns nil for nil' do
       expect(subject_module.identify(nil)).to be nil
+    end
+  end
+
+  describe '.build' do
+    let(:subject_class) { Class.new(Typekit::Element::Base) }
+    let(:client) { double }
+
+    before(:example) do
+      allow(Typekit::Element).to receive(:classify).and_return(subject_class)
+    end
+
+    subject { subject_module.build(:cat, client) }
+
+    it 'creates a new Element class' do
+      expect(subject < subject_class).to be true
+    end
+
+    it 'creates a new Element class whose instances have a Client embedded' do
+      expect(subject.new.client).to be client
     end
   end
 end
