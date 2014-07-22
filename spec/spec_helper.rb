e2d0c104d2c202fc379900f9c175ae56d8ea3146
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-07-22 06:25:59 +0200

    Moved spec/cassettes into spec/fixture/cassette

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |config|
-  config.cassette_library_dir = 'spec/cassettes'
+  config.cassette_library_dir = 'spec/fixtures/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.default_cassette_options = {
